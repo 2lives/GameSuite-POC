e0469befc39a534acd0bb87976120963966fc24a
Brandon Liu <brandon.slipjet@gmail.com>
2018-05-12 16:42:37 -0700

    fortnite db pulling from api correctly and populating mongo.

--- imports/apis/fortniteAPI.js
+++ imports/apis/fortniteAPI.js
@@ -5,25 +5,41 @@ import { HTTP } from 'meteor/http';
 
 const Fortnite = new Mongo.Collection('fornite');
 
-Meteor.methods({
-    //user
-    'fortnite.getProfile'(user) {
-        console.log('hello');
-        HTTP.call(
-            'GET',
-            //template literals for user
-            `https://api.fortnitetracker.com/v1/profile/pc/calvintyvm`,
-            {
-                headers: {
-                    'TRN-Api-Key': 'e5c756b6-f460-45e3-9653-d878c0a535dc'
-                }
-            },
-            (err, result) => {
-                if (!err) Fortnite.insert(result);
-            }
-        );
+// Meteor.methods({
+//     //user
+//     'fortnite.getProfile'(user) {
+//         console.log('hello');
+//         HTTP.call(
+//             'GET',
+//             //template literals for user
+//             `https://api.fortnitetracker.com/v1/profile/pc/calvintyvm`,
+//             {
+//                 headers: {
+//                     'TRN-Api-Key': 'e5c756b6-f460-45e3-9653-d878c0a535dc'
+//                 }
+//             },
+//             (err, result) => {
+//                 if (!err) Fortnite.insert(result);
+//             }
+//         );
+//     }
+// });
+
+HTTP.call(
+    'GET',
+    'https://api.fortnitetracker.com/v1/profile/pc/calvintyvm',
+    {
+        headers: {
+            'TRN-Api-Key': 'e5c756b6-f460-45e3-9653-d878c0a535dc'
+        }
+    },
+    (error, result) => {
+        if (!error) {
+            console.log(result);
+            Fortnite.insert(result);
+        }
     }
-});
+);
 
 //async await
 
