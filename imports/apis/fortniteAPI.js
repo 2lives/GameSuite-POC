a670c786de6b705c2fdbc299d11fd9ca083c5073
Brandon Liu <brandon.slipjet@gmail.com>
2018-05-15 13:56:24 -0700

    profile info in fortnite strong object accessible.

--- imports/apis/fortniteAPI.js
+++ imports/apis/fortniteAPI.js
@@ -6,68 +6,27 @@ import { HTTP } from 'meteor/http';
 const Fortnite = new Mongo.Collection('fortnite');
 
 // Meteor.methods({
-//     //user
-//     'fortnite.getProfile'(user) {
-//         console.log('hello');
-//         HTTP.call(
-//             'GET',
-//             //template literals for user
-//             `https://api.fortnitetracker.com/v1/profile/pc/calvintyvm`,
-//             {
-//                 headers: {
-//                     'TRN-Api-Key': 'e5c756b6-f460-45e3-9653-d878c0a535dc'
-//                 }
-//             },
-//             (err, result) => {
-//                 if (!err) Fortnite.insert(result);
-//             }
-//         );
-//     }
+
 // });
 
-// HTTP.call(
-//     'GET',
-//     'https://api.fortnitetracker.com/v1/profile/pc/calvintyvm',
-//     {
-//         headers: {
-//             'TRN-Api-Key': 'e5c756b6-f460-45e3-9653-d878c0a535dc'
-//         }
-//     },
-//     (error, result) => {
-//         if (!error) {
-//   Fortnite.update(
-//       { user: 'calvintyvm' },
-//       { $set: { profile: result.content } },
-//       { upsert: true }
-//   );
-//         }
-//     }
-// );
-
-// async function
-const fetchAsync = async () => {
-    let response = await HTTP.call(
-        'GET',
-        'https://api.fortnitetracker.com/v1/profile/pc/calvintyvm',
-        {
-            headers: {
-                'TRN-Api-Key': 'e5c756b6-f460-45e3-9653-d878c0a535dc'
-            }
+HTTP.call(
+    'GET',
+    'https://api.fortnitetracker.com/v1/profile/pc/calvintyvm',
+    {
+        headers: {
+            'TRN-Api-Key': 'e5c756b6-f460-45e3-9653-d878c0a535dc'
         }
-    );
-    let data = await response;
-    return data;
-};
-
-fetchAsync()
-    .then(data =>
-        Fortnite.update(
-            { user: 'calvintyvm' },
-            { $set: { profile: result.content } },
-            { upsert: true }
-        )
-    )
-    .catch(reason => console.log(reason.message));
+    },
+    (error, result) => {
+        if (!error) {
+            Fortnite.update(
+                { user: 'calvintyvm' },
+                { $set: { profile: result.content } },
+                { upsert: true }
+            );
+        }
+    }
+);
 
 //async await
 // console.log(Fortnite.find().fetch());
