2d11c70216a60861d26a8ea8afa3f15055ecf7ad
Brandon Liu <brandon.slipjet@gmail.com>
2018-05-17 22:14:03 -0700

    changed server main.js to try and get fortnite login.

--- imports/ui/components/FortniteLogin/FortniteLogin.js
+++ imports/ui/components/FortniteLogin/FortniteLogin.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
-import { Meteor } from 'meteor/meteor';
-
-const UserVal = 'calvintyvm';
 class FortniteLogin extends Component {
     constructor(props) {
         super(props);
         this.state = { value: '' };
-
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
     handleChange(event) {
         this.setState({ value: event.target.value });
     }
-
     handleSubmit(event) {
         console.log('An Epic Games name was submitted: ' + this.state.value);
         event.preventDefault();
-        Meteor.users.update(
-            { id: /*this.state.value*/ UserVal },
-            { $set: { 'services.fortnite.id': /*this.state.value*/ UserVal } }
-        );
+        // Meteor.users.update(
+        //   { _id: Meteor.userId() },
+        //   { $set: { 'services.fortnite.id': /*this.state.value*/ UserVal } }
+        // );
+        FortniteUpdate(this.state.value);
     }
-
     render() {
         return (
             <div>
@@ -41,5 +35,4 @@ class FortniteLogin extends Component {
         );
     }
 }
-
 export default FortniteLogin;
--- server/main.js
+++ server/main.js
@@ -6,50 +6,50 @@ import mainServer from '../imports/start-up/server';
 import SteamProfile from '../imports/apis/steamAPI';
 
 Meteor.startup(() => {
-  ServiceConfiguration.configurations.upsert(
-    { service: 'steam' },
-    {
-      $set: {
-        loginStyle: 'popup',
-        timeout: 10000
-      }
-    }
-  );
-  //   ServiceConfiguration.configurations.upsert(
-  //     { service: 'battlenet' },
-  //     {
-  //       $set: {
-  //         clientId: '8gxaf95jd99wm2xgt9hdkrxqpwm95fex',
-  //         scope: 'sc2.profile',
-  //         secret: 'W4EAyCKRRhjJKdMjpfX6kASydrYtdkaG'
-  //       }
-  //     }
-  //   );
+    ServiceConfiguration.configurations.upsert(
+        { service: 'steam' },
+        {
+            $set: {
+                loginStyle: 'popup',
+                timeout: 10000
+            }
+        }
+    );
+    //   ServiceConfiguration.configurations.upsert(
+    //     { service: 'battlenet' },
+    //     {
+    //       $set: {
+    //         clientId: '8gxaf95jd99wm2xgt9hdkrxqpwm95fex',
+    //         scope: 'sc2.profile',
+    //         secret: 'W4EAyCKRRhjJKdMjpfX6kASydrYtdkaG'
+    //       }
+    //     }
+    //   );
 });
 
 /**
  * Adds steam user profile to existing logged in meteor account (if it exists)
  */
 Accounts.validateNewUser(user => {
-  if (user.services.steam) {
-    Meteor.users.update(
-      { _id: Meteor.userId() },
-      {
-        $set: { 'services.steam': user.services.steam }
-      },
-      { upsert: true }
-    );
-    return false;
-  } else {
-    return true;
-  }
+    if (user.services.steam) {
+        Meteor.users.update(
+            { _id: Meteor.userId() },
+            {
+                $set: { 'services.steam': user.services.steam }
+            },
+            { upsert: true }
+        );
+        return false;
+    } else {
+        return true;
+    }
 });
 /**
  * Adds fortnite data to user object
  */
 export const FortniteUpdate = event => {
-  Meteor.users.update(
-    { _id: Meteor.userId() },
-    { $set: { 'services.fortnite.id': this.state.value } }
-  );
+    Meteor.users.update(
+        { _id: Meteor.userId() },
+        { $set: { 'services.fortnite.id': this.state.value } }
+    );
 };
