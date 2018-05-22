05fd5fd37e10a26993c4f9efac6a86afa60c385f
Eric Chan <ericchan277@gmail.com>
2018-05-21 20:59:31 -0700

    Merge conflict

--- imports/ui/containers/GeneralChat.js
+++ imports/ui/containers/GeneralChat.js
@@ -3,24 +3,44 @@ import TextField from 'material-ui/TextField';
 import { withTracker } from 'meteor/react-meteor-data';
 
 class GeneralChatContainer extends Component {
-    render() {
-        return (
-            <div>
-                <p>Chat goes here</p>
-                <TextField
-                    hintText="Compose Message"
-                    floatingLabelText="Chat with us!"
-                />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+    this.handleSubmitMessage = this.handleSubmitMessage.bind(this);
+    this.handleChangeMessage = this.handleChangeMessage.bind(this);
+  }
+  handleChangeMessage(event) {
+    this.setState({ message: event.target.value });
+  }
+
+  handleSubmitMessage(e) {
+    e.preventDefault();
+    const submittedMessage = this.state.message;
+    Meteor.call('Meteor.message.PostMessage', submittedMessage);
+  }
+  render() {
+    return (
+      <div>
+        <p>Chat goes here</p>
+        <form onSubmit={this.handleSubmitMessage}>
+          <TextField
+            onChange={this.handleChangeMessage}
+            hintText="Compose Message"
+            floatingLabelText="Chat with us!"
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 const GeneralChat = withTracker(() => {
-    Meteor.subscribe('messages');
-    return {
-        messages: Messages.find().fetch()
-    };
+  Meteor.subscribe('messages');
+  return {
+    messages: Messages.find().fetch()
+  };
 })(GeneralChatContainer);
 
 export default GeneralChat;
